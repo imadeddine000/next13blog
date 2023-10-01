817d21da1f568d204a175815699374e51e1bdf05
imadeddinekebour <imadeddinekebourlite@gmail.com>
2023-10-01 11:17:06 +0100

    adding connection string

--- server/mongodb.js
+++ server/mongodb.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 const createConncetion=async ()=>{
     try {
-        await mongoose.connect('mongodb://localhost:27017/nextjsblog')
+        await mongoose.connect('mongodb+srv://imad:imad@cluster0.7iu2s4z.mongodb.net/?retryWrites=true&w=majority')
         console.log('connected!')
     }catch(err){
         console.log('connection error')
